2926c9432c879b2da97ac0e53168433b99f3ce70
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-05-26 10:56:45 +0200

    rename the project

---
+++ SerialTempSens.cs
@@ -0,0 +1,55 @@
+﻿using FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using System.IO;
+
+namespace FanControl.SerialTempSensPlugin
+{
+    internal class SerialTempSens: IPluginSensor
+    {
+        public SerialTempSens(SerialPort serialPort) { _serialPort = serialPort; }
+
+        private readonly uint _sensIndex = 1; // TODO set index
+
+        public float? Value { get; private set; }
+
+        public string Name => $"NTC Sensor {(int)_sensIndex + 1}";
+
+        public string Id => "Sens_" + _sensIndex.ToString();
+
+        public void Update()
+        {
+            Command cmd = new Command { SensorId = _sensIndex };
+
+            byte[] msg;
+            using (var stream = new MemoryStream())
+            {
+                cmd.WriteDelimitedTo(stream);
+                msg = stream.ToArray();
+            }
+
+            _serialPort.Write(msg, 0, msg.Length);
+
+            try
+            {
+                var len = _serialPort.ReadByte();
+
+                byte[] rply = new byte[len];
+                _serialPort.Read(rply, 0, len);
+
+                Reply reply = new Reply();
+                reply.MergeFrom(rply);
+
+                Value = reply.Temperature / 1000.0F;   // Data is in m°C
+            }
+            catch (TimeoutException) { }
+        }
+
+        private SerialPort _serialPort;
+    }
+}
---
+++ SerialTempSensPlugin.cs
@@ -0,0 +1,46 @@
+﻿using FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.IO.Ports;
+
+namespace FanControl.SerialTempSensPlugin
+{
+    public class SerialTempSensPlugin : IPlugin
+    {
+        private bool _isInitialised = false;
+        private SerialPort _serialPort;
+
+        public string Name => "SerialTempSens";
+
+        public void Initialize()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 115200;
+            _serialPort.PortName = "COM5";  // TODO detect port somehow
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.Open();
+            _isInitialised = true;
+        }
+
+        public void Load(IPluginSensorsContainer _container)
+        {
+            if (_isInitialised)
+            {
+                SerialTempSens sensor = new SerialTempSens(_serialPort);
+                _container.TempSensors.Add(sensor);
+            }
+        }
+
+        public void Close()
+        {
+            if (_isInitialised)
+            {
+                _serialPort.Close();
+                _isInitialised = false;
+            }
+        }
+    }
+}
