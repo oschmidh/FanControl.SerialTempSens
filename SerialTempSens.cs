52f1377766f9cc2a2caface38a5ec0f261ae4746
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-07-09 19:46:13 +0200

    Autodetect available sensor IDs on Load()

--- SerialTempSens.cs
+++ SerialTempSens.cs
@@ -8,15 +8,29 @@ namespace FanControl.SerialTempSensPlugin
 {
     internal class SerialTempSens : IPluginSensor
     {
-        public SerialTempSens(SerialPort serialPort) { _serialPort = serialPort; }
+        public SerialTempSens(SerialPort serialPort, uint sensIndex)
+        {
+            _serialPort = serialPort;
+            _sensIndex = sensIndex;
+        }
 
-        private readonly uint _sensIndex = 0; // TODO set index
+        private readonly uint _sensIndex;
 
         public float? Value { get; private set; }
 
         public string Name => $"NTC Sensor {(int)_sensIndex + 1}";
 
         public string Id => "Sens_" + _sensIndex.ToString();
+
+        public bool IsPresent()
+        {
+            Command cmd = new Command { SensorId = _sensIndex };
+            Reply? rply = sendCommand(cmd);
+            if (rply == null) {
+                return false;
+            }
+            return rply.Error != ErrorCode.SensorOpen && rply.Error != ErrorCode.InvalidSensorId;
+        }
 
         public void Update()
         {
@@ -44,6 +58,31 @@ namespace FanControl.SerialTempSensPlugin
                 Value = reply.Temperature / 1000.0F;   // Data is in m°C
             }
             catch (TimeoutException) { }
+        }
+
+        private Reply? sendCommand(Command cmd)
+        {
+            byte[] msg;
+            using (var stream = new MemoryStream())
+            {
+                cmd.WriteDelimitedTo(stream);
+                msg = stream.ToArray();
+            }
+
+            _serialPort.Write(msg, 0, msg.Length);
+
+            try
+            {
+                var len = _serialPort.ReadByte();
+
+                byte[] rply = new byte[len];
+                _serialPort.Read(rply, 0, len);
+
+                Reply reply = new Reply();
+                reply.MergeFrom(rply);
+                return reply;
+            }
+            catch (TimeoutException) { return null;}
         }
 
         private SerialPort _serialPort;
